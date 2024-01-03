0735474ca2fae2c082095cf439a1b223d45f0ac5
Rong-Tao <trsjtu17@gmail.com>
2024-01-03 17:31:20 +0800

    Thrust Device now working

--- Crawler/Thrust/findIntersection.cu
+++ Crawler/Thrust/findIntersection.cu
@@ -1,30 +1,22 @@
-#include <iostream> // Required for std::cout
+#include <iostream>
 #include <stdio.h>
 #include <thrust/set_operations.h>
 #include <thrust/functional.h>
 #include <thrust/execution_policy.h>
+#include <thrust/device_vector.h>
 
-int main() { // Corrected syntax here
+extern "C" {
+    int launchFindIntersection(int* list1, int* list2, int list1Size, int list2Size, int* result) {
+        thrust::device_vector<int> d_list1(list1, list1 + list1Size);
+        thrust::device_vector<int> d_list2(list2, list2 + list2Size);
+        thrust::device_vector<int> d_result(list1Size + list2Size);
 
-    int A1[7] = {12, 10, 8, 6, 4, 2, 0};
-    int A2[5] = {9, 7, 5, 3, 1};
-    int result[12]; // Adjusted size to 12, since 7 + 5 = 12
-
-    // thrust::set_union returns an iterator to the end of the result range
-    int *result_end = thrust::set_union(thrust::host, A1, A1 + 7, A2, A2 + 5, result, thrust::greater<int>());
-
-    // Calculate the size of the result
-    int result_size = result_end - result;
-
-    // Print the result
-    std::cout << "Union result: [";
-    for (int i = 0; i < result_size; ++i) {
-        std::cout << result[i];
-        if (i < result_size - 1) {
-            std::cout << ", ";
-        }
+        auto d_result_end = thrust::set_union(thrust::device, 
+                                            d_list1.begin(), d_list1.end() , 
+                                            d_list2.begin(), d_list2.end() , 
+                                            d_result.begin(), 
+                                            thrust::greater<int>());
+        thrust::copy(d_result.begin(), d_result_end, result);
+        return d_result_end - d_result.begin(); 
     }
-    std::cout << "]" << std::endl;
-
-    return 0;
-}
+} 
